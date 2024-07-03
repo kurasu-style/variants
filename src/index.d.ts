392565a2ec9c28050d368ababeb1fa10aa6b0f92
dawidseipold <seipold.dawid@gmail.com>
2024-07-03 12:23:46 +0200

    feat: add type declaration file for index.ts

---
+++ src/index.d.ts
@@ -0,0 +1,31 @@
+type VariantsMap = {
+  [key: string]: { [key: string]: string };
+};
+
+type DefaultMap<V extends VariantsMap> = {
+  [K in keyof V]: keyof V[K];
+};
+
+type VariantOptions<V extends VariantsMap> = {
+  [K in keyof V]?: keyof V[K];
+};
+
+type CompoundVariant<V extends VariantsMap> = (options: {
+  [K in keyof V]: keyof V[K];
+}) => string | undefined;
+
+export class Variants<V extends VariantsMap, D extends DefaultMap<V>> {
+  base: string;
+  variants: V;
+  default: D;
+  compoundVariants?: CompoundVariant<V>[];
+
+  constructor(config: {
+    base: string;
+    variants: V;
+    default: D;
+    compoundVariants?: CompoundVariant<V>[];
+  });
+
+  forgeClasses(options?: VariantOptions<V>): string;
+}
